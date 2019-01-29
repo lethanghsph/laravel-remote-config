4a369a4b934b56718c2a5321eb9bde79c178c99b
thangle <lethanghsph@gmail.com>
2019-01-29 15:09:20 +0700

    Init laravel package

---
+++ src/LaravelRemoteConfigServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lethanghsph\LaravelRemoteConfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRemoteConfigServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       //
+    }
+}
