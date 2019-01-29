de70b4534c8c5fa877e8cb3dc9daeac759f9ba5c
thangle <lethanghsph@gmail.com>
2019-01-30 01:07:38 +0700

    Update get_remote_config function

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -8,22 +8,21 @@ if (!function_exists('get_remote_config')) {
      *
      * @param string $configKey
      * @param string $configPath
+     * @param null   $default
      * @return mixed
      */
-    function get_remote_config(string $configKey, string $configPath)
+    function get_remote_config(string $configKey, string $configPath, $default = null)
     {
         $configs = app(RemoteConfig::class)->get($configPath);
 
-        try {
+        if (isset($configs['propertySources']) && is_array($configs['propertySources'])) {
+            $configKey = 'acme.' . $configKey;
             foreach ($configs['propertySources'] as $config) {
-                $configKey = 'acme.' . $configKey;
                 if (isset($config['source'][$configKey])) {
                     return $config['source'][$configKey];
                 }
             }
-            return null;
-        } catch (\Exception $e) {
-            return null;
+            return $default;
         }
     }
-};
+}
