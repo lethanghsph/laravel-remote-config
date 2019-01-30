2726ee4f6065227185e75f11125a72adf46cb485
thangle <lethanghsph@gmail.com>
2019-01-30 08:49:40 +0700

    Update config env

--- src/Services/RequestClient.php
+++ src/Services/RequestClient.php
@@ -20,12 +20,12 @@ class RequestClient
     public function __construct(array $config = null)
     {
         $this->config = [
-            "host"     => config("laravel-remote-config.host"),
-            "user"     => config("laravel-remote-config.user"),
-            "password" => config("laravel-remote-config.password"),
-            "timeout"  => config("laravel-remote-config.timeout", 5),
-            "env"      => config("laravel-remote-config.env"),
-            "prefix"   => config("laravel-remote-config.prefix"),
+            "host"     => env("EXTERNAL_SERVICE_HOST"),
+            "user"     => env("EXTERNAL_SERVICE_USER"),
+            "password" => env("EXTERNAL_SERVICE_PASSWORD"),
+            "timeout"  => env("EXTERNAL_SERVICE_TIMEOUT", 5),
+            "env"      => env("EXTERNAL_SERVICE_ENV"),
+            "prefix"   => env("EXTERNAL_SERVICE_URI_PREFIX"),
         ];
 
         if (!is_null($config)) {
